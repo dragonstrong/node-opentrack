b4a4a7831109c171913f36ba4f48d60aedd52d13
dragonstrong <32926995+dragonstrong@users.noreply.github.com>
2021-09-14 09:04:57 +0800

    Update client.js

--- examples/client.js
+++ examples/client.js
@@ -1,5 +1,5 @@
 const OpenTrack = require('opentrack')
-const client = new OpenTrack.Client('127.0.0.1', 4243)
+const client = new OpenTrack.Client('10.128.200.231', 4243)
 
 // Called every time data is sent
 client.onUpdate((transform, delta) => {
